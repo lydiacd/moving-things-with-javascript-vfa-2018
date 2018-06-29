b8b1d82fe87e9e744473c1607537971c00b635a4
Lydia Dick <lydiadick@gmail.com>
2018-06-29 19:04:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = '#FF69B4'
 
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    var rightNumbers = dodger.style.right.replace('px', '')
+    var right = parseInt(rightNumbers, 10)
+ 
+    dodger.style.right = `${right + 1}px`
+  }
+})
